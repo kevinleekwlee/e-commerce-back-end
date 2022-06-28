4a412f4fdae15a2e750c6dc48eff0fb776109f90
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-06-28 02:43:08 -0400

    Added product-routes.js and tag-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,8 +8,10 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
-    res.status(200).json(productData);
+    const productsData = Product.findAll({
+      include: [{ model: Category },{ model: Tag }]
+    });
+    res.status(200).json(productsData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -19,6 +21,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProductData = Product.findByPk(req.params.id, {
+      include: [{ model: Category },{ model: Tag }]
+    });
+
+    if (!singleProductData) {
+      res.status(404).json({ message: 'This product ID does not exist!' });
+      return;
+    }
+
+    res.status(200).json(singleProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -97,6 +113,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const singleProductData = Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!singleProductData) {
+      res.status(404).json({ message: 'This product ID does not exist!' });
+      return;
+    }
+
+    res.status(200).json(singleProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
